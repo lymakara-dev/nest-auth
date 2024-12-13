8d3f8e2903bae66db9b997d10a277db4d2f7ceda
lymakara-dev <ly.makara.personal@gmail.com>
2024-12-13 20:39:11 +0700

    Key changes: - install jwt to tgenerate the real access token - create config forlder to keep the file that contain the jwt secret   token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { log } from 'console';
 
 @Controller('auth')
 export class AuthController {
@@ -15,6 +16,7 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() input: { username: string; password: string }) {
+    log('input from postman: ' + input.username);
     return this.authService.authenticate(input);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 type AuthInput = { username: string; password: string };
@@ -7,7 +8,10 @@ type AuthResult = { accessToken: string; userId: number; username: string };
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async authenticate(input: AuthInput): Promise<AuthResult> {
     const user = await this.validateUser(input);
@@ -17,11 +21,13 @@ export class AuthService {
         'kom jg login username password ort trov teh',
       );
     }
-    return {
-      accessToken: 'fake-access',
-      userId: user.userId,
-      username: user.username,
-    };
+    // return {
+    //   accessToken: 'fake-access',
+    //   userId: user.userId,
+    //   username: user.username,
+    // };
+
+    return this.signIn(user);
   }
 
   async validateUser(input: AuthInput): Promise<SignInData | null> {
@@ -31,5 +37,16 @@ export class AuthService {
       return { userId: user.userId, username: user.username };
     }
     return null;
+  }
+
+  async signIn(user: SignInData): Promise<AuthResult> {
+    const tokenPayload = {
+      sub: user.userId,
+      username: user.username,
+    };
+
+    const accessToken = await this.jwtService.signAsync(tokenPayload);
+
+    return { accessToken, username: user.username, userId: user.userId };
   }
 }
